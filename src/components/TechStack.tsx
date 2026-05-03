026170a85202e344ab2f1027522709352bd7f6b7
Utkarsh Verma <Utkarshverma121101@gmail.com>
2026-05-03 20:23:02 +0530

    new techstacks

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -13,14 +13,14 @@ import {
 
 const textureLoader = new THREE.TextureLoader();
 const imageUrls = [
-  "/images/react2.webp",
-  "/images/next2.webp",
-  "/images/node2.webp",
-  "/images/express.webp",
-  "/images/mongo.webp",
-  "/images/mysql.webp",
-  "/images/typescript.webp",
-  "/images/javascript.webp",
+  "/images/API.webp",
+  "/images/docker.webp",
+  "/images/PG 5.webp",
+  "/images/spring.webp",
+  "/images/Github.webp",
+  "/images/Redis.webp",
+  "/images/Jenkins.webp",
+  "/images/java.webp",
 ];
 const textures = imageUrls.map((url) => textureLoader.load(url));
 
